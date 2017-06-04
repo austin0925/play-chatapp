7b4c1ec213d44d37e1fff13fdd87b64965297748
austin0925 <austinmsn@hotmail.com>
2017-06-05 06:10:31 +0800

    01_07 00:18

---
+++ app.js
@@ -0,0 +1,13 @@
+var express = require("express");
+var app = express();
+
+app.use(express.static("public"));
+app.use(express.static("node_modules/bootstrap/dist"));
+
+app.get('/hello', function(req, res){
+    res.send('hello worlds, make better worlds!');
+});
+
+app.listen(3000, function(){
+    console.log('Chat app listening on port 3000!');
+});
