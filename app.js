4340075ac32ee536547771db00e226414dccf9b6
austin0925 <austinmsn@hotmail.com>
2017-06-06 22:16:32 +0800

    ch2-11 00:29

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 var express = require("express");
 var app = express();
 
+app.set("views", "./views");
+app.set('view engine', 'jade');
+
 app.use(express.static("public"));
 app.use(express.static("node_modules/bootstrap/dist"));
 
-app.get('/hello', function(req, res){
-    res.send('hello worlds, make better worlds!');
+app.get('/', function(req, res){
+    // res.send('hello worlds, make better worlds!');
+    res.render("index", { title: "Home"});
+});
+
+app.get('/admin/rooms', function(req, res){
+    // res.send('hello worlds, make better worlds!');
+    res.render("rooms", { title: "Admin Rooms"});
 });
 
 app.listen(3000, function(){
