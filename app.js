ed6a12b36cf82c3a2332173d38e71c7b4a8f3a94
austin0925 <austinmsn@hotmail.com>
2017-06-08 05:04:23 +0800

    add some chat rooms on CH0306 00:03

--- app.js
+++ app.js
@@ -2,12 +2,15 @@ var express = require("express");
 var app = express();
 
 var rooms = require("./data/rooms.json");
+var bodyParser = require("body-parser");
+var uuid = require("node-uuid");
 
 app.set("views", "./views");
 app.set('view engine', 'jade');
 
 app.use(express.static("public"));
 app.use(express.static("node_modules/bootstrap/dist"));
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/', function handler(req, res){
     // res.send('hello worlds, make better worlds!');
@@ -29,7 +32,15 @@ app.get('/admin/rooms/add', function(req, res){
 });
 
 app.post('/admin/rooms/add', function(req, res){
-    res.render("nothing");
+    var room = {
+        name: req.body.name,
+        id: uuid.v4()
+    };
+
+    rooms.push(room);
+
+    res.json(room);
+    //res.render("nothing");
 });
 
 app.listen(3000, function(){
