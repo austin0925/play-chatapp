1a798bd99288ea5ca214fb1d91274e33f113711a
austin0925 <austinmsn@hotmail.com>
2017-06-11 21:33:10 +0800

    ch08-03 end

--- admin.js
+++ admin.js
@@ -5,6 +5,7 @@ var express = require("express");
 var router = express.Router();
 
 var rooms = require("./data/rooms.json");
+
 module.exports = router;
 
 router.get('/rooms', function (req, res) {
@@ -15,51 +16,46 @@ router.get('/rooms', function (req, res) {
     });
 });
 
-router.get('/rooms/add', function (req, res) {
-    res.render("add");
-});
+router.route('/rooms/add')
+    .get(function (req, res) {
+        res.render("add");
+    })
 
-router.post('/rooms/add', function (req, res) {
-    var room = {
-        name: req.body.name,
-        id: uuid.v4()
-    };
+    .post(function (req, res){
 
-    rooms.push(room);
+        var room = {
+            name: req.body.name,
+            id: uuid.v4()
+        };
 
-    res.redirect(req.baseUrl + "/rooms");
-    //res.json(room);
-    //res.render("nothing");
-});
+        rooms.push(room);
 
-router.get('/rooms/edit/:id', function (req, res) {
-    var roomId = req.params.id;
+        res.redirect(req.baseUrl + "/rooms");   //abssu
+        // res.redirect("./");  //this will redirect the page to the relative self dir.
+    });
 
-    var room = _.find(rooms, r => r.id === roomId);
+router.route('/rooms/edit/:id')
+    .all(function(req, res, next){
+        var roomId = req.params.id;
 
-    if (!room) {
-        res.sendStatus(404);
-        return;
-    }
+        var room = _.find(rooms, r => r.id === roomId);
+        if (!room) {
+            // res.sendStatus(404);
+            next(new Error("Oh NO"));
+            return;
+        }
+        res.locals.room = room;
+        next();
+    })
+    .get(function (req, res) {
+        res.render("edit");
+    })
+    .post(function (req, res) {
+        res.locals.room.name = req.body.name;
 
-    res.render("edit", {room});
-})
+        res.redirect(req.baseUrl + "/rooms");
+    });
 
-router.post('/rooms/edit/:id', function (req, res) {
-    var roomId = req.params.id;
-
-    var room = _.find(rooms, r => r.id === roomId);
-
-    if (!room) {
-        res.sendStatus(404);
-        return;
-    }
-    room.name = req.body.name;
-
-    res.redirect(req.baseUrl + "/rooms");
-    //res.json(room);
-    //res.render("nothing");
-});
 
 router.get("/rooms/delete/:id", function (req, res) {
     var roomId = req.params.id;
@@ -68,4 +64,4 @@ router.get("/rooms/delete/:id", function (req, res) {
 
     res.redirect(req.baseUrl + "/rooms");
     // res.json(roomId);
-})
+});
--- app.js
+++ app.js
@@ -1,25 +1,52 @@
 var express = require("express");
 var app = express();
-
 var bodyParser = require("body-parser");
 
 app.set("views", "./views");
 app.set('view engine', 'jade');
 
+var fs = require("fs");
+var accessLogStream = fs.createWriteStream(__dirname + '/access.log', {flags: 'a'});
+
+app.use(require('morgan')("combined", {stream: accessLogStream}));
+
 app.use(express.static("public"));
 app.use(express.static("node_modules/bootstrap/dist"));
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("node_modules/jquery/dist"));
 
-app.get('/', function handler(req, res){
-    // res.send('hello worlds, make better worlds!');
-    res.render("index", { title: "Home"}, function (error, html){
-        console.log(html);
-    });
+require('express-debug')(app, {});
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+
+app.use(function (req, res, next) {
+    console.log(`Incoming request: ${req.url}`);
+    next();
+});
+
+app.get('/', function (req, res, next) {
+    throw new Error("oh no");
+    console.log(`Incoming request: second`);
+    res.render("home", {title: "Home"});
+    next();
 });
 
 var adminRouter = require("./admin");
 app.use("/admin", adminRouter);
+app.use("/admin", function (req, res, next) {
+    console.log('admin request: ' + req.url);
+    next();
+})
 
-app.listen(3000, function(){
+var apiRouter = require('./api');
+app.use("/api", apiRouter);
+
+app.use(function (error, req, res, next) {
+    console.error(error);
+    res.send("Super secret error handler.")
+});
+
+app.listen(3000, function () {
     console.log('Chat app listening on port 3000!');
 });
