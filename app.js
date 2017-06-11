1830a1fe37387f46508ea484236282fb1d6151f6
austin0925 <austinmsn@hotmail.com>
2017-06-11 11:24:07 +0800

    ch05-07 end

--- app.js
+++ app.js
@@ -1,22 +1,37 @@
 var express = require("express");
 var app = express();
 
+var rooms = require("./data/rooms.json");
+
 app.set("views", "./views");
 app.set('view engine', 'jade');
 
 app.use(express.static("public"));
 app.use(express.static("node_modules/bootstrap/dist"));
 
-app.get('/', function(req, res){
+app.get('/', function handler(req, res){
     // res.send('hello worlds, make better worlds!');
-    res.render("index", { title: "Home"});
+    res.render("index", { title: "Home"}, function (error, html){
+        console.log(html);
+    });
 });
 
 app.get('/admin/rooms', function(req, res){
     // res.send('hello worlds, make better worlds!');
-    res.render("rooms", { title: "Admin Rooms"});
+    res.render("rooms", {
+        title: "Admin Rooms",
+        rooms: rooms
+    });
 });
 
-app.listen(3000, function(){
-    console.log('Chat app listening on port 3000!');
+app.get('/admin/rooms/add', function(req, res){
+    res.render("add");
+});
+
+app.post('/admin/rooms/add', function(req, res){
+    res.render("nothing");
+});
+
+app.listen(3001, function(){
+    console.log('Chat app listening on port 3001!');
 });
